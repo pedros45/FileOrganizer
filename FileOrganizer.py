a22f76f691349c11fb6f15328d36aa5481694d70
pedros45 <pedrosuazo10@gmail.com>
2020-02-11 21:25:07 -0800

    Final Commit. Organization works as intended, and readme has been updated with correct way to run the program. Currently the program creates and empty directory due to hidden OS Files (.DS_STORE etc) which is an issue I am working on still.

--- FileOrganizer.py
+++ FileOrganizer.py
@@ -5,52 +5,55 @@ import shutil
 import argparse
 
 
+def moveFiles(currentDirectory, currentOs, onlyFiles):
+
+    # Create Directories in current folder for each month and then moves the file into the appropriate folder
+    for file in onlyFiles:
+        fileData = ""
+
+        # Turn timestamp into MetaData String depending on OS
+        if currentOs == "Windows":
+            fileData = time.ctime(os.path.getctime(os.path.join(currentDirectory, file))).split(" ")
+        elif currentOs == "Darwin":
+            fileData = time.ctime(os.stat(os.path.join(currentDirectory, file)).st_birthtime).split(" ")
+
+        # Path for possible newDir
+        newDir = os.path.join(currentDirectory, fileData[1] + fileData[len(fileData) - 1])
+
+        # Path for the current file being processed
+        currFile = os.path.join(currentDirectory, file)
+
+        # If the proper directory does not exist, make it
+        if not os.path.exists(newDir):
+            os.mkdir(newDir)
+
+        # Move the file into its appropriate directory
+        shutil.move(currFile, newDir)
+
+
 def organize(orgDir):
-    # get the current directory from which the user is running the script
-    currentDirectory = os.getcwd()
+    # get the path to the user home directory
+    start = os.path.expanduser("~")
 
     # what OS is the user using?
     currentOs = platform.system()
 
     for folder in orgDir:
+        currentDirectory = os.path.join(start, folder)
 
-        # adjust the path to OneDrive folder appropriately depending on user OS
-        if currentOs == "Windows":
-            currentDirectory = currentDirectory + "\\" + folder
-        elif currentOs == "Darwin":
-            currentDirectory = currentDirectory + "/" + folder
-        else:
-            print("Currently doesn't support Linux distributions...")
+        onlyDirectories = [d for d in os.listdir(currentDirectory) if os.path.isdir(os.path.join(currentDirectory, d))]
 
-        print("Processing files. Please wait....")
+        if len(onlyDirectories) != 0:
+            for dir in onlyDirectories:
+                subDir = os.path.join(currentDirectory, dir)
+                subDirFiles = [f for f in os.listdir(subDir) if os.path.isfile(os.path.join(subDir, f))]
+                moveFiles(subDir, currentOs, subDirFiles)
 
+        # Organize files in the base directory
         onlyFiles = [f for f in os.listdir(currentDirectory) if os.path.isfile(os.path.join(currentDirectory, f))]
+        moveFiles(currentDirectory, currentOs, onlyFiles)
 
-        # Create Directories in OneDrive Folder for each month and then moves the file into the appropriate folder
-        for file in onlyFiles:
-
-            fileData = ""
-
-            # Turn timestamp into MetaData String depending on OS
-            if currentOs == "Windows":
-                fileData = time.ctime(os.path.getctime(os.path.join(currentDirectory, file))).split(" ")
-            elif currentOs == "Darwin":
-                fileData = time.ctime(os.stat(os.path.join(currentDirectory, file)).st_birthtime).split(" ")
-
-            # Path for possible newDir
-            newDir = os.path.join(currentDirectory, fileData[1] + fileData[len(fileData) - 1])
-
-            # Path for the current file being processed
-            currFile = os.path.join(currentDirectory, file)
-
-            # If the proper directory does not exist, make it
-            if not os.path.exists(newDir):
-                os.mkdir(newDir)
-
-            # Move the file into its appropriate directory
-            shutil.move(currFile, newDir)
-
-    print("The following directories " + orgDir + " have been organized!")
+    print("The following directories " + str(orgDir) + " have been organized!")
 
 
 # Create ArgumentParser object to hold all the information to parse from the command line
--- README.md
+++ README.md
@@ -1,45 +1,32 @@
-# FileOrganizer (Not Complete)
+# FileOrganizer
 
-A Python script turned command line tool that automates file organization based on the creation date of the files. Using the terminal, the user is able to change into the directory of their choice and then provide a list of directories whose files will be organized. 
+A Python script turned command line tool that automates file organization based on the creation date of the files. Using the terminal, the user is able to provide a list of paths to the directories whose files or subdirectories will be organized. 
 
 ## Getting Started
 
 In order to get started, clone the project from my GitHub into any location you'd like on your computer.
 ### Prerequisites
 
-Make sure to have at least Python 3.7 installed. Nothing beyond the standard libraries are used so as long as you have the correct version of Python the program should run correctly.
-
-### Installing
-
-After doing so, go to the cloned repository from your terminal and run the Initializer.py file as such
- ```
-python Initializer.py
-``` 
-or 
-```
-python3 Initializer.py
-```
-After doing so, the current working directory should be added as one of your PATH environment variables. After doing so the FileOrganizer.py file will be executable from any directory you wish to run from. 
+Make sure to have at least Python 3.7 installed. 
 
 ### Example
 
-Let's say you wanted to organize your OneDrive directory.
-Change into that directory: 
-```
-cd OneDrive
-```
-
-Once you are in that directory, call the FileOrganizer.py file along with a list of sub-directories, delimited by a space, that exist inside the OneDrive directory that need to be organized as such:
+Let's say you wanted to organize your OneDrive directory and some other folder.
+Change into the cloned repository on your computer.
+Once there, you can call the script. The arguments that get passed with the script
+are paths relative to your HOME DIRECTORY e.g ``` /User/(your username)/``` for MacOs or ``` C:\Users\(your username)\ ``` for Windows.
+Call the FileOrganizer.py file along with a list of the relative paths of the directories, delimited by a space, that you would like organized.
 
 ```
-python FileOrganizer.py Pictures OtherPictures 
+python FileOrganizer.py OneDrive path/to/another/folder
 ```
 
 Once run you should get a confirmation message stating that the folders were organized successfully. Checking the directories you should see new folders within it that now hold the pictures.
+It should be noted that the script also checks the directory for subdirectories and organizes them as well following the same criteria.
 ## Authors
 
 * **Pedro Suazo** -- Github: [pedros45](https://github.com/pedros45)
 
 ## Acknowledgments
 
-* Thank you to my girlfriend for giving me the idea for this project and for inspiring me to make it as cool and as useful as possible! I hope this ends up being useful not only to her, but to others who may be interested in using it!
+* Thank you to my girlfriend for giving me the idea for this project and for inspiring me to make it as cool and as useful as possible! I hope this ends up being useful not only to her, but to others as well!
